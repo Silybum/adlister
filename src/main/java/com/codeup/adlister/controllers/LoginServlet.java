98c9682678bac52e9783bcbe23a8bfe4d18f981b
Cierra Smith <cierra.r.smith3@gmail.com>
2021-09-17 14:29:41 -0500

    intended redirects fixed

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -25,7 +25,8 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        String redirect = (String) request.getSession().getAttribute("redirect");
+        String redirect = (String) request.getSession().getAttribute("redirect"); // this is null because it is not pulling from a session. Same as null.equals()
+
 
         User user = DaoFactory.getUsersDao().findByUsername(username);
 
@@ -34,21 +35,21 @@ public class LoginServlet extends HttpServlet {
             return;
         }
 
+        if (redirect == null) {
+            redirect = " ";
+        }
+
         boolean validAttempt = Password.check(password, user.getPassword());
-
-
 
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
-
             if (redirect.equals("create")) {
                 response.sendRedirect("/ads/create");
             } else {
                 response.sendRedirect("/profile");
             }
-        } else {
-//            alert("user/ password combo didn't match ");
-            response.sendRedirect("/login");
+            } else {
+                response.sendRedirect("/login");
         }
     }
 }
